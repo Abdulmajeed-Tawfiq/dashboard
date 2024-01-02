4dcc32cb3610915cfc772805e0a146aabe0040e2
Abdulmajeed-Tawfiq <abdulmajeedtawfiq98@gmail.com>
2024-01-02 13:55:27 +0300

    make the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import styles from "./ui/home/home.module.css";
+
 export default function Home() {
   return (
-    <main className="">
-      <h2>HomePage</h2>
-    </main>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Welcome to the Dashboard!</h1>
+      <p className={styles.subtitle}>
+        Manage your products, users, transactions, and settings.
+      </p>
+      <nav className={styles.nav}>
+        <Link href="/dashboard" className={styles.navItem}>
+          Go to Dashboeard
+        </Link>
+      </nav>
+    </div>
   );
 }
